ddc59c1b8aae8be85403cc3b342ab0b082400407
synabs <synabsai@gmail.com>
2026-06-06 22:44:46 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './styles/globals.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Demo from './components/Demo'
 
 export default function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Demo />
     </>
   )
 }
