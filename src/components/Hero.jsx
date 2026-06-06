d9a6e52beb4ead653f00eafa2fd91f4650ad23fe
synabs <synabsai@gmail.com>
2026-06-06 19:13:07 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -65,7 +65,7 @@ export default function Hero() {
             display: 'block',
             borderRadius: '8px',
             boxShadow: '0 0 0 1px rgba(0,0,0,0.15)',
-            transform: 'scale(1.6)',
+            transform: 'scale(1.28)',
             transformOrigin: 'center center',
           }}
         />
