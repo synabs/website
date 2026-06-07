c63a7a166c844a7b0dd5ae7627e56578e3096d4d
synabs <synabsai@gmail.com>
2026-06-07 03:06:41 +0300

    Update Evolve.jsx

--- src/components/Evolve.jsx
+++ src/components/Evolve.jsx
@@ -40,7 +40,7 @@ export default function Evolve() {
           <div className="evolve-card__image">
             <img src="/website.avif" alt="" className="evolve__img" />
           </div>
-          <h3 className="evolve-card__title">Works any website</h3>
+          <h3 className="evolve-card__title">Works on any website</h3>
           <p className="evolve-card__text">
             Compatible with WordPress, Shopify and custom websites. Works with a single line of code.
           </p>
