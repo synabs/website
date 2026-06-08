28bd62083480289c2c07a2c076b78719c406ef0a
synabs <synabsai@gmail.com>
2026-06-08 17:36:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './styles/globals.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Features from './components/Features'
 import Demo from './components/Demo'
 import Evolve from './components/Evolve'
 import Pricing from './components/Pricing'
