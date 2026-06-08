bf3163f70e21631e24089d02fb769626fc87ff2b
synabs <synabsai@gmail.com>
2026-06-08 20:17:45 +0300

    Create Setup.css

---
+++ src/styles/Setup.css
@@ -0,0 +1,19 @@
+import '../styles/Setup.css'
+
+export default function Setup() {
+  return (
+    <section className="setup">
+      <div className="setup__fullbleed">
+        <img src="/setup.avif" alt="" className="setup__bg" />
+        <div className="setup__overlay">
+          <p className="evolve-stats__label evolve-stats__label--light">SETUP</p>
+          <h2 className="setup__title">2 minute install</h2>
+          <p className="setup__text">
+            We give you a single line of code. Add it to your website yourself, or we do it together. AI agent starts running instantly and you never have to touch it again.
+          </p>
+          <a href="/join" className="evolve__cta">Try free 14 days</a>
+        </div>
+      </div>
+    </section>
+  )
+}
