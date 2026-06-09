1f6e13bd459cf9ea22b1dc966623b92a1b9c8806
synabs <synabsai@gmail.com>
2026-06-09 17:03:19 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './styles/globals.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Demo from './components/Demo'
+import Setup from './components/Setup'
+import Evolve from './components/Evolve'
+import Prototype from './components/Prototype'
+import About from './components/About'
+import Pricing from './components/Pricing'
+import CTA from './components/CTA'
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Setup />
+      <About />
+      <Pricing />
+      <CTA />
+    </>
+  )
+}
