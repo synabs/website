6e5d290e681f770330b877a25496d48eb75f822c
synabs <synabsai@gmail.com>
2026-06-11 12:54:17 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,10 @@ export default function App() {
       <Navbar />
       <Hero />
       <FeaturesResults />
+      <Cortex />
       <Abilities />
       <SetupSection />
       <About />
-      <Cortex />
       <Pricing />
       <CTA />
     </>
