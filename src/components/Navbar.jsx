b700480fe791517a8fe9d544bbf3b9cafac74678
synabs <synabsai@gmail.com>
2026-06-16 13:31:00 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,10 +41,6 @@ export default function Navbar() {
             ))}
           </ul>
 
-          <a href="/trial" className="navbar__trial-link">
-            Start free trial
-          </a>
-
           <button
             className={`navbar__hamburger${menuOpen ? ' navbar__hamburger--open' : ''}`}
             onClick={toggleMenu}
@@ -73,6 +69,10 @@ export default function Navbar() {
               />
             </svg>
           </button>
+
+          <a href="/trial" className="navbar__trial-link">
+            Start free trial
+          </a>
         </div>
       </div>
 
