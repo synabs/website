fa11c144bb0a29453916c6d1ee13826ec6a7a321
synabs <synabsai@gmail.com>
2026-06-16 13:25:41 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,17 +31,16 @@ export default function Navbar() {
           />
         </a>
 
-        {/* Desktop-linkit – keskellä, vain kun menu auki */}
-        <ul className={`navbar__desktop-list${menuOpen ? ' navbar__desktop-list--open' : ''}`}>
-          {navLinks.map(({ href, label }) => (
-            <li key={href}>
-              <a href={href} className="navbar__desktop-link">{label}</a>
-            </li>
-          ))}
-        </ul>
-
-        {/* Oikealle: Start free trial -nappi + hamburger */}
+        {/* Oikealle: linkit + Start free trial -nappi + hamburger */}
         <div className="navbar__right">
+          <ul className={`navbar__desktop-list${menuOpen ? ' navbar__desktop-list--open' : ''}`}>
+            {navLinks.map(({ href, label }) => (
+              <li key={href}>
+                <a href={href} className="navbar__desktop-link">{label}</a>
+              </li>
+            ))}
+          </ul>
+
           <a href="/trial" className="navbar__trial-link">
             Start free trial
           </a>
