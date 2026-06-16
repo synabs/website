127516c77b8bb4161e25af7757751ecf4c9e9892
synabs <synabsai@gmail.com>
2026-06-16 13:47:21 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './styles/globals.css'
 import Navbar from './components/Navbar'
+import Scrollprogress from './components/Scrollprogress'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import Abilities from './components/Abilities'
@@ -20,6 +21,7 @@ export default function App() {
   return (
     <>
       <Navbar />
+      <Scrollprogress />
       <Hero />
       <FeaturesResults />
       <Cortex />
